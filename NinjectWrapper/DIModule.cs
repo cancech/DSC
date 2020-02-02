7a0d1645f83696907ff85e97c7b6a4a529463cc0
Jaroslav Bosak <cancech@gmail.com>
2020-02-02 15:41:31 -0500

    Copying in the ninject wrapper.

---
+++ NinjectWrapper/DIModule.cs
@@ -0,0 +1,16 @@
+﻿namespace DependencyInjection
+{
+    public abstract class DIModule : Ninject.Modules.NinjectModule
+    {
+
+        protected void BindConstant<I>(I instance, string name)
+        {
+            Bind<I>().ToConstant(instance).Named(name);
+        }
+
+        protected void BindType<I, T>() where T : I
+        {
+            Bind<I>().To<T>();
+        }
+    }
+}
