3e78c4d6ef6326bf6a3b0cb55959d2dc494e0313
Jaroslav Bosak <cancech@gmail.com>
2020-02-08 15:31:56 -0500

    Adding the GUI elements.

--- DSC/MainWindow.cs
+++ DSC/MainWindow.cs
@@ -6,6 +6,12 @@ public partial class MainWindow : Gtk.Window
     public MainWindow() : base(Gtk.WindowType.Toplevel)
     {
         Build();
+
+        Pango.FontDescription fontDesc = new Pango.FontDescription
+        {
+            Size = Convert.ToInt32(24 * Pango.Scale.PangoScale)
+        };
+        outputLabel.ModifyFont(fontDesc);
     }
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
