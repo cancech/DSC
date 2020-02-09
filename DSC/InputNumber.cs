750a0534c8f9c9393656ecec7ca81035019e58a0
Jaroslav Bosak <cancech@gmail.com>
2020-02-09 10:11:00 -0500

    Adding a representation for the number input by the user.

---
+++ DSC/InputNumber.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace DSC
+{
+    public class InputNumber
+    {
+        private string input;
+        private bool isDecimal;
+        private bool isPositive;
+        private int signFactor;
+
+        public InputNumber()
+        {
+            // Clear will reset all values to their expected initial state
+            Clear();
+        }
+
+        public void AppendDigit(int digit)
+        {
+            //Don't append leading 0's
+            if (digit == 0 && IsEmpty())
+                return;
+
+            input += digit;
+        }
+
+        public void AppendDecimalPoint()
+        {
+            // Ignore subsequent decimal inputs. Only care about the first one
+            if (isDecimal)
+                return;
+
+            input += ".";
+            isDecimal = true;
+        }
+
+        public void InvertSign()
+        {
+            isPositive = !isPositive;
+            signFactor = signFactor * -1;
+        }
+
+        public void DeleteDigit()
+        {
+            // Only delete if there is something to delete
+            if (IsEmpty())
+                return;
+
+            String removed = input.Substring(input.Length - 1);
+            input = input.Remove(input.Length - 1);
+
+            // Check if the element removed is the decimal point
+            if (removed.Equals("."))
+                isDecimal = false;
+        }
+
+        public void Clear()
+        {
+            input = "";
+            isDecimal = false;
+            isPositive = true;
+            signFactor = 1;
+        }
+
+        public string ValueString()
+        {
+            string toReturn = input;
+
+            // Massage the resulting string
+            if (IsEmpty())
+                toReturn = "0";
+            else if (input.StartsWith(".", StringComparison.Ordinal))
+                toReturn = 0 + toReturn;
+            if (!isPositive)
+                toReturn = "-" + toReturn;
+
+            return toReturn;
+        }
+
+        public bool IsDecimal()
+        {
+            return isDecimal;
+        }
+
+        public int ValueInt()
+        {
+            if (isDecimal || IsEmpty())
+                return 0;
+
+            return Int32.Parse(input) * signFactor;
+        }
+
+        public double ValueDouble()
+        {
+            if (!isDecimal || IsEmpty() || input.Equals("."))
+                return 0.0;
+
+            return Double.Parse(input) * signFactor;
+        }
+
+        private bool IsEmpty()
+        {
+            return input.Equals("");
+        }
+    }
+}
---
+++ DSCTest/InputNumberTest.cs
@@ -0,0 +1,263 @@
+﻿using NUnit.Framework;
+using DSC;
+
+namespace DSCTest
+{
+    [TestFixture()]
+    public class InputNumberTest
+    {
+
+        private InputNumber input;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            input = new InputNumber();
+        }
+
+        [Test]
+        public void TestBaseNumber()
+        {
+            AssertValue("0", 0, 0.0, false);
+        }
+
+        [Test]
+        public void TestLeadingZeros()
+        {
+            // First try just the one
+            input.AppendDigit(0);
+            AssertValue("0", 0, 0.0, false);
+
+            // Now try a bunch
+            input.AppendDigit(0);
+            input.AppendDigit(0);
+            input.AppendDigit(0);
+            input.AppendDigit(0);
+            input.AppendDigit(0);
+            input.AppendDigit(0);
+            input.AppendDigit(0);
+            input.AppendDigit(0);
+            input.AppendDigit(0);
+            AssertValue("0", 0, 0.0, false);
+        }
+
+        [Test]
+        public void TestIntInput()
+        {
+            input.AppendDigit(1);
+            AssertValue("1", 1, 0.0, false);
+            input.AppendDigit(2);
+            AssertValue("12", 12, 0.0, false);
+            input.AppendDigit(3);
+            AssertValue("123", 123, 0.0, false);
+            input.AppendDigit(4);
+            AssertValue("1234", 1234, 0.0, false);
+            input.AppendDigit(5);
+            AssertValue("12345", 12345, 0.0, false);
+        }
+
+        [Test]
+        public void TestFollowingZeros()
+        {
+            input.AppendDigit(9);
+            AssertValue("9", 9, 0.0, false);
+            input.AppendDigit(0);
+            AssertValue("90", 90, 0.0, false);
+            input.AppendDigit(0);
+            AssertValue("900", 900, 0.0, false);
+            input.AppendDigit(0);
+            AssertValue("9000", 9000, 0.0, false);
+            input.AppendDigit(0);
+            AssertValue("90000", 90000, 0.0, false);
+        }
+
+        [Test]
+        public void TestDeleteInt()
+        {
+            // Check that nothing happens if were delete when nothing is present
+            input.DeleteDigit();
+            AssertValue("0", 0, 0.0, false);
+
+            // Add one digit and delete it
+            input.AppendDigit(6);
+            AssertValue("6", 6, 0.0, false);
+            input.DeleteDigit();
+            AssertValue("0", 0, 0.0, false);
+            // Still nothing happens if we delete "past" all digits
+            input.DeleteDigit();
+            AssertValue("0", 0, 0.0, false);
+
+            // Now add/delete a bunch of things
+            input.AppendDigit(7);
+            AssertValue("7", 7, 0.0, false);
+            input.AppendDigit(8);
+            AssertValue("78", 78, 0.0, false);
+            input.AppendDigit(9);
+            AssertValue("789", 789, 0.0, false);
+            input.DeleteDigit();
+            AssertValue("78", 78, 0.0, false);
+            input.AppendDigit(0);
+            AssertValue("780", 780, 0.0, false);
+            input.DeleteDigit();
+            AssertValue("78", 78, 0.0, false);
+            input.AppendDigit(4);
+            AssertValue("784", 784, 0.0, false);
+            input.AppendDigit(1);
+            AssertValue("7841", 7841, 0.0, false);
+            input.DeleteDigit();
+            AssertValue("784", 784, 0.0, false);
+            input.DeleteDigit();
+            AssertValue("78", 78, 0.0, false);
+            input.DeleteDigit();
+            AssertValue("7", 7, 0.0, false);
+            input.DeleteDigit();
+            AssertValue("0", 0, 0.0, false);
+            input.DeleteDigit();
+            AssertValue("0", 0, 0.0, false);
+            input.DeleteDigit();
+            AssertValue("0", 0, 0.0, false);
+            input.DeleteDigit();
+            AssertValue("0", 0, 0.0, false);
+        }
+
+        [Test]
+        public void TestDecimal()
+        {
+            input.AppendDigit(5);
+            AssertValue("5", 5, 0.0, false);
+            input.AppendDecimalPoint();
+            AssertValue("5.", 0, 5.0, true);
+            input.AppendDigit(5);
+            AssertValue("5.5", 0, 5.5, true);
+            input.AppendDigit(5);
+            AssertValue("5.55", 0, 5.55, true);
+            input.AppendDigit(5);
+            AssertValue("5.555", 0, 5.555, true);
+        }
+
+        [Test]
+        public void TestDeleteDecimal()
+        {
+            // Input some number
+            input.AppendDigit(8);
+            AssertValue("8", 8, 0, false);
+            input.AppendDecimalPoint();
+            AssertValue("8.", 0, 8.0, true);
+            input.AppendDigit(1);
+            AssertValue("8.1", 0, 8.1, true);
+            input.AppendDigit(6);
+            AssertValue("8.16", 0, 8.16, true);
+
+            // Now delete!
+            input.DeleteDigit();
+            AssertValue("8.1", 0, 8.1, true);
+            input.DeleteDigit();
+            AssertValue("8.", 0, 8.0, true);
+            input.DeleteDigit();
+            AssertValue("8", 8, 0, false);
+            input.DeleteDigit();
+            AssertValue("0", 0, 0, false);
+        }
+
+        [Test]
+        public void TestClear()
+        {
+            // Get some integer input in place
+            TestIntInput();
+            AssertValue("12345", 12345, 0.0, false);
+            // Clear it
+            input.Clear();
+            AssertValue("0", 0, 0.0, false);
+
+            // Get some decimal in place
+            TestDecimal();
+            AssertValue("5.555", 0, 5.555, true);
+            // Clear it
+            input.Clear();
+            AssertValue("0", 0, 0.0, false);
+        }
+
+        [Test]
+        public void TestInvertSignNoInput()
+        {
+            AssertValue("0", 0, 0.0, false);
+            input.InvertSign();
+            AssertValue("-0", 0, 0.0, false);
+            input.InvertSign();
+            AssertValue("0", 0, 0.0, false);
+        }
+
+        [Test]
+        public void TestInvertSignInt()
+        {
+            input.InvertSign();
+            AssertValue("-0", 0, 0.0, false);
+            input.AppendDigit(7);
+            AssertValue("-7", -7, 0.0, false);
+            input.AppendDigit(3);
+            AssertValue("-73", -73, 0.0, false);
+            input.InvertSign();
+            AssertValue("73", 73, 0.0, false);
+        }
+
+        [Test]
+        public void TestClearNegativeInt()
+        {
+            // Get some integer input in place
+            TestIntInput();
+            AssertValue("12345", 12345, 0.0, false);
+            input.InvertSign();
+            AssertValue("-12345", -12345, 0.0, false);
+            // Clear it
+            input.Clear();
+            AssertValue("0", 0, 0.0, false);
+
+            // Make sure that it "stays" positive
+            input.AppendDigit(9);
+            AssertValue("9", 9, 0.0, false);
+        }
+
+        [Test]
+        public void TestClearNegativeDecimal()
+        {
+            // Get some decimal in place
+            TestDecimal();
+            AssertValue("5.555", 0, 5.555, true);
+            input.InvertSign();
+            AssertValue("-5.555", 0, -5.555, true);
+
+            // Clear it
+            input.Clear();
+            AssertValue("0", 0, 0.0, false);
+
+            // Make sure that it "stays" positive
+            input.AppendDigit(8);
+            AssertValue("8", 8, 0.0, false);
+        }
+
+        [Test]
+        public void TestFirstInputDecimalPoint()
+        {
+            // Number can start with just the decimal point
+            input.AppendDecimalPoint();
+            AssertValue("0.", 0, 0.0, true);
+            input.AppendDigit(4);
+            AssertValue("0.4", 0, 0.4, true);
+
+            // Can delete everything properly
+            input.DeleteDigit();
+            AssertValue("0.", 0, 0.0, true);
+            input.DeleteDigit();
+            AssertValue("0", 0, 0.0, false);
+        }
+
+        private void AssertValue(string txt, int intVal, double dblVal, bool isDecimal)
+        {
+            Assert.AreEqual(txt, input.ValueString());
+            Assert.AreEqual(intVal, input.ValueInt());
+            Assert.AreEqual(dblVal, input.ValueDouble());
+            Assert.AreEqual(isDecimal, input.IsDecimal());
+        }
+    }
+}
