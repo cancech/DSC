522b6b8e563ea8a50fb93c32e822422c546c4e8f
Jaroslav Bosak <cancech@gmail.com>
2020-02-13 14:15:15 -0500

    Refactoring the main method to allow for easier automated testing.

--- DSC/Program.cs
+++ DSC/Program.cs
@@ -2,30 +2,36 @@
 
 namespace DSC
 {
-    class MainClass
+    class Program
     {
-        public static void Main(string[] args)
-        {
-            // The model which is to be shared
-            CalcModel calcModel = new CalcModel();
 
+        public Program(CalcModel model)
+        {
             // The calculator core
             Calculator cal = new Calculator();
-            RegisterCommandProcessors(cal, calcModel);
+            RegisterCommandProcessors(cal, model);
 
-            Application.Init();
-            MainWindow win = new MainWindow(calcModel);
-            win.Show();
-            Application.Run();
         }
 
-        private static void RegisterCommandProcessors(Calculator cal, CalcModel model)
+        private void RegisterCommandProcessors(Calculator cal, CalcModel model)
         {
             CommandManager manager = new CommandManager(model);
             manager.RegisterCommand(new NumericCommandProcessor(cal));
             manager.RegisterCommand(new OperationCommandProcessor(cal));
             manager.RegisterCommand(new EqualsCommandProcessor(cal));
             manager.RegisterCommand(new ClearCommandProcessor(cal));
+        }
+
+        public static void Main(string[] args)
+        {
+            // The model which is to be shared
+            CalcModel calcModel = new CalcModel();
+            Program program = new Program(calcModel);
+
+            Application.Init();
+            MainWindow win = new MainWindow(calcModel);
+            win.Show();
+            Application.Run();
         }
     }
 }
